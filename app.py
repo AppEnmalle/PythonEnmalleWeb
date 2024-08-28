17501be6fdd65bdb3fc7db2a30c949a0658a5250
AppEnmalle <cristian.ma.ev@gmail.com>
2024-08-28 17:49:24 -0500

    Asistencia y Nuevo Creyentel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
 import requests
 import os
 
@@ -7,9 +8,10 @@ app.secret_key = os.environ.get('SECRET_KEY', 'supersecretkey')  # Usa una clave
 
 API_LOGIN_URL = 'https://apirestnodeenmalle.onrender.com/acceso/login'
 API_ASISTENCIA_URL = 'https://apirestnodeenmalle.onrender.com/celula/listado'
-API_REGISTRO_ASISTENCIA_URL = 'https://apirestnodeenmalle.onrender.com/celula/registro/asistencia'
+API_REGISTRO_ASISTENCIA_URL = 'https://apirestnodeenmalle.onrender.com/celula/registro/asistencia/web'
 API_REGISTRO_NUEVO_URL = 'https://apirestnodeenmalle.onrender.com/celula/nuevo/creyente'
 API_BARRIOS_URL = 'https://apirestnodeenmalle.onrender.com/celula/listado/sector'
+API_CONTROL_REGISTRO_ASISTENCIA_URL = 'https://apirestnodeenmalle.onrender.com/celula/control/registro/asistencia'
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -39,26 +41,46 @@ def menu():
     user = session['user']
     return render_template('menu.html', user=user)
 
-@app.route('/asistencia', methods=['POST'])
+# Ruta para asistencia
+@app.route('/asistencia', methods=['POST', 'GET'])
 def asistencia():
     if 'user' not in session:
         return redirect(url_for('login'))
 
-    lider = request.form.get('lider')
-    response = requests.post(API_ASISTENCIA_URL, json={'lider': lider})
 
-    if response.status_code == 200:
-        asistencia_data = response.json()
+    print ("metodo request ", request.method)
+    if request.method == 'GET' or request.method == 'POST':
+        #lider = request.form.get('lider')
+        lider = session['user']['lider']
+        lider_str = str(lider)
+        request.method = 'POST'
+        print ("metodo request JJJJ", request.method)
 
-        # Verificar si la respuesta está vacía
-        if not asistencia_data:
-            # Si no hay datos, redirige con un mensaje
-            return render_template('asistencia.html', mensaje='Usted ya registró la asistencia esta semana.')
-        
-        total_items = len(asistencia_data)
-        return render_template('asistencia.html', asistencia=asistencia_data, total_items=total_items, mensaje=None)
-    else:
-        return "Error al obtener asistencia", response.status_code
+        response = requests.post(API_ASISTENCIA_URL, json={'lider': lider_str})
+        print ("//////////////////////////////////")
+        print (lider)
+        print (response)
+
+        if response.status_code == 200:
+            asistencia_data = response.json()
+
+            # Verificar si la respuesta está vacía
+            if not asistencia_data:
+                # Si no hay datos, redirige con un mensaje
+                return render_template('asistencia.html', mensaje='Usted ya registró la asistencia esta semana.')
+
+            # Formatear las fechas
+            for item in asistencia_data:
+                # Convertir de ISO 8601 a datetime y luego a formato d-m-Y
+                item['fecha_nac'] = datetime.strptime(item['fecha_nac'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d-%m-%Y')
+
+            total_items = len(asistencia_data)
+            return render_template('asistencia.html', asistencia=asistencia_data, total_items=total_items, mensaje=None)
+        else:
+            return "Error al obtener asistencia", response.status_code
+
+    return render_template('asistencia.html', mensaje=None)
+
 
 @app.route('/guardar_asistencia', methods=['POST'])
 def guardar_asistencia():
@@ -73,10 +95,11 @@ def guardar_asistencia():
         return redirect(url_for('asistencia'))
 
     trama = f"{lider};{celula}:{','.join(ids_seleccionados)},"
+    print (trama)
 
     # Enviar la trama a la API de registro de asistencia
     response = requests.post(API_REGISTRO_ASISTENCIA_URL, json={'trama': trama})
-
+    print (response.status_code)
     if response.status_code == 200:
         # Devuelve un mensaje de éxito en formato JSON
         return {'message': 'Se registró correctamente su asistencia', 'status': 'success'}
@@ -137,6 +160,81 @@ def nuevo():
             return render_template('nuevo.html', mensaje='Error al enviar la solicitud.', error=str(e), barrios=barrios_data)
 
     return render_template('nuevo.html', barrios=barrios_data)
+
+@app.route('/listado')
+def listado():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    user = session['user']
+    return render_template('listado.html', user=user)
+
+@app.route('/estadistica')
+def estadistica():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    user = session['user']
+    return render_template('estadistica.html', user=user)
+
+@app.route('/cumpleaños')
+def cumpleaños():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    user = session['user']
+    return render_template('cumpleaños.html', user=user)
+
+
+@app.route('/validacionasistencia', methods=['GET', 'POST'])
+def validacionasistencia():
+    print("Entro////////AQUI")
+    print(request.method)
+    
+    if request.method == 'POST':
+        # Obtener datos de la sesión
+        lider = session.get('user', {}).get('lider')
+        celula = session.get('user', {}).get('celula')
+        
+        if lider is None or celula is None:
+            return "Error: Datos de sesión incompletos", 400
+
+        lider_str = str(lider)
+        celula_str = str(celula)
+        print("Lider:", lider_str)
+        print("Celula:", celula_str)
+
+        try:
+            response = requests.post(API_CONTROL_REGISTRO_ASISTENCIA_URL, json={'lider': lider_str, 'celula': celula_str})
+            response.raise_for_status()
+            print(response)
+            data = response.json()
+            print("Datos recibidos de la API:", data)
+
+            # Asegúrate de que `data` es una lista y accede al primer elemento
+            if isinstance(data, list):
+                if len(data) == 0:
+                    print("Fecha está vacía, redirigiendo a asistencia")
+                    return redirect(url_for('asistencia'))
+                else:
+                    fecha = list(data[0].values())[0]  # Obtén el primer valor del primer diccionario
+                    print("Fecha:", fecha)
+
+                    if fecha == "Ene_01_1900":
+                        print("Fecha es 'Ene_01_1900', redirigiendo a menu")
+                        return redirect(url_for('menu'))
+                    else:
+                        return render_template('validacionasistencia.html', fecha=fecha)
+            else:
+                return "Error: La respuesta de la API no es una lista", 400
+
+        except requests.RequestException as e:
+            return f"Error al contactar con la API: {e}"
+
+    return render_template('menu.html')
+
+
+
+
+
+
 
 
 
